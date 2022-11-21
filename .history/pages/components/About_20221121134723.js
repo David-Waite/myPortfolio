a7ad888881bedc0960d079b7214f618ab89a3351
David-Waite <davidwaite122@gmail.com>
2022-11-21 15:16:41 +1100

    mobile view test

---
+++ .history/pages/components/About_20221121134723.js
@@ -0,0 +1,39 @@
+import styles from "../../styles/about.module.css";
+
+export default function About() {
+  return (
+    <div>
+      <img className={styles.background} src="/background.png" alt="" />
+      <div className={styles.about}>
+        <div className={styles.aboutMe}>
+          <h2>About me</h2>
+          <p>
+            I like to create simple clean websites that gets the user to where
+            they wanna go without the clutter.
+            <br />
+            <br />
+            I&apos;m currently studying frontend web development at Swinburne
+            University in Melbourne, with the plans of doing computer science in
+            2023.
+          </p>
+        </div>
+        <div className={styles.aboutTech}>
+          <h2>
+            Technology&apos;s
+            <br />
+            <strong>I&apos;m comfortable in</strong>
+          </h2>
+          <div className={styles.techCon}>
+            <img src="/React-icon.png" alt="" />
+            <img src="/nextJsIcon.png" alt="" />
+            <img src="/contentfulIcon.png" alt="" />
+            <img src="/JsIcon.png" alt="" />
+          </div>
+        </div>
+        <h1>
+          <a href=""></a>
+        </h1>
+      </div>
+    </div>
+  );
+}
---
+++ .history/pages/components/About_20221121140018.js
@@ -0,0 +1,40 @@
+import styles from "../../styles/about.module.css";
+
+export default function About() {
+  return (
+    <div
+      className={styles.container}
+      style={{ backgroundImage: "url('/background.png'" }}
+    >
+      <div className={styles.about}>
+        <div className={styles.aboutMe}>
+          <h2>About me</h2>
+          <p>
+            I like to create simple clean websites that gets the user to where
+            they wanna go without the clutter.
+            <br />
+            <br />
+            I&apos;m currently studying frontend web development at Swinburne
+            University in Melbourne, with the plans of doing computer science in
+            2023.
+          </p>
+        </div>
+        <div></div>
+        <div></div>
+        <div className={styles.aboutTech}>
+          <h2>
+            Technology&apos;s
+            <br />
+            <strong>I&apos;m comfortable in</strong>
+          </h2>
+          <div className={styles.techCon}>
+            <img src="/React-icon.png" alt="" />
+            <img src="/nextJsIcon.png" alt="" />
+            <img src="/contentfulIcon.png" alt="" />
+            <img src="/JsIcon.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
