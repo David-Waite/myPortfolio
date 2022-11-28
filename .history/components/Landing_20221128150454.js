9a249b0ae7ea57b7fe25d9e427ad9538f879948a
David-Waite <davidwaite122@gmail.com>
2022-11-28 15:16:01 +1100

    added function to the form

--- .history/components/Landing_20221128150454.js
+++ .history/components/Landing_20221128150454.js
@@ -81,22 +81,22 @@ export default function Landing() {
               className={`${styles.backArrow} ${styles.arrowPath}`}
               d="M51 47L59.9293 55.9293C59.9683 55.9683 60.0317 55.9683 60.0707 55.9293L69 47"
               stroke="#3AB8FF"
-              strokeWidth="5"
-              strokeLinecap="round"
+              stroke-width="5"
+              stroke-linecap="round"
             />
             <path
               className={`${styles.midArrow} ${styles.arrowPath}`}
               d="M51 47L59.9293 55.9293C59.9683 55.9683 60.0317 55.9683 60.0707 55.9293L69 47"
               stroke="#3AB8FF"
-              strokeWidth="5"
-              strokeLinecap="round"
+              stroke-width="5"
+              stroke-linecap="round"
             />
             <path
               className={`${styles.frontArrow} ${styles.arrowPath}`}
               d="M51 64L59.9293 72.9293C59.9683 72.9683 60.0317 72.9683 60.0707 72.9293L69 64"
               stroke="#3AB8FF"
-              strokeWidth="5"
-              strokeLinecap="round"
+              stroke-width="5"
+              stroke-linecap="round"
             />
           </g>
         </svg>
