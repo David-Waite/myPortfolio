7f29190fe3f2ff909cff0e041d7d536884e41b91
David-Waite <davidwaite122@gmail.com>
2023-01-16 13:01:27 +1100

    recapcha test

--- .history/pages/api/contact_20230116130046.js
+++ .history/pages/api/contact_20230116130046.js
@@ -3,9 +3,9 @@ import sendgrid from "@sendgrid/mail";
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendEmail(req, res) {
-  if (req.method === "POST") {
-    try {
-      await fetch("https://www.google.com/recaptcha/api/siteverify", {
+   if (req.method === "POST") {
+   await try {
+      fetch("https://www.google.com/recaptcha/api/siteverify", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
@@ -20,24 +20,23 @@ async function sendEmail(req, res) {
           );
           if (reCaptchaRes?.score > 0.5) {
             try {
-              // console.log("REQ.BODY", req.body);
-              sendgrid.send({
-                to: process.env.PERSONAL_EMAIL_ADDRESS, // Your email where you'll receive emails
-                from: process.env.EMAIL_ADDRESS, // your website email address here
-                subject: `MESSAGE VIA PORTFOLIO FROM: ${req.body.name}`,
-                html: `
+    // console.log("REQ.BODY", req.body);
+     sendgrid.send({
+      to: process.env.PERSONAL_EMAIL_ADDRESS, // Your email where you'll receive emails
+      from: process.env.EMAIL_ADDRESS, // your website email address here
+      subject: `MESSAGE VIA PORTFOLIO FROM: ${req.body.name}`,
+      html: `
       <div>
         <h2>Name: ${req.body.name}</h2
         <h3>Email: ${req.body.email}</h3>
         <h3>Message: ${req.body.message}</h3>
       </div>`,
-              });
-            } catch (error) {
-              // console.log(error);
-              return res
-                .status(error.statusCode || 500)
-                .json({ error: error.message });
-            }
+    });
+  } catch (error) {
+    // console.log(error);
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
+           
           } else {
             res.status(200).json({
               status: "failure",
